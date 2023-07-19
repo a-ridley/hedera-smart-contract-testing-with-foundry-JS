eee1a2d76158b804f6589f8f1ac249fc3169e07b
Abi Castro <abixcastro@gmail.com>
2023-07-19 15:25:32 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+import { Client, AccountId, PrivateKey, Hbar } from "@hashgraph/sdk";
+import fs from "fs";
+import dotenv from "dotenv";
+import { deployContract } from "./services/hederaSmatContractService.js";
+ 
+dotenv.config();
+// create your client
+const accountIdString = process.env.OPERATOR_ACCOUNT_ID;
+const privateKeyString = process.env.OPERATOR_PRIVATE_KEY;
+
+if (accountIdString === undefined || privateKeyString === undefined ) { throw new Error('account id and private key in env file are empty')}
+
+ 
+const operatorAccountId = AccountId.fromString(accountIdString);
+const address = operatorAccountId.toSolidityAddress();
+const operatorPrivateKey = PrivateKey.fromString(privateKeyString);
+ 
+const client = Client.forTestnet().setOperator(operatorAccountId, operatorPrivateKey);
+client.setDefaultMaxTransactionFee(new Hbar(100));
+
+// read the bytecode
+const bytecode = fs.readFileSync("binaries/contracts_TodoList_sol_TodoList.bin");
+
+const hederaFoundryExample = async () => {    
+    // Deploy contract
+    const gasLimit = 1000000;
+    const [contractId] = await deployContract(client, bytecode, gasLimit);
+    console.log(`Check it out on Hashscan!`);
+    console.log(`https://hashscan.io/testnet/contract/${contractId}`);
+    console.log(address);
+
+    client.close();
+}
+
+hederaFoundryExample();
